8b0297e93acbe88b2c6d5a8a5c38846a82c41084
Kaisikk <Kosikov.Ilya@mail.ru>
2024-05-10 18:37:20 +0400

    Initial commit

---
+++ src/main/java/com/kaisikk/java/kaisikmtscourse/KaisikMtsCourseApplication.java
@@ -0,0 +1,13 @@
+package com.kaisikk.java.kaisikmtscourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaisikMtsCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KaisikMtsCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kaisikk/java/kaisikmtscourse/KaisikMtsCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaisikk.java.kaisikmtscourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaisikMtsCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
