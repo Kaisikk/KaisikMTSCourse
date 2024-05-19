2f1dd8d694fafe5b6597b2756e7ef29bff0a72d7
Kaisikk <Kosikov.Ilya@mail.ru>
2024-05-19 20:26:42 +0400

    Работа с циклическим барьером

---
+++ src/main/java/com/kaisikk/java/kaisikmtscourse/JavaCyclicBarrier.java
@@ -0,0 +1,44 @@
+package com.kaisikk.java.kaisikmtscourse;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class JavaCyclicBarrier {
+
+    public static void main(String[] args) {
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(3, new Run());
+
+        new SportsMan(cyclicBarrier);
+        new SportsMan(cyclicBarrier);
+        new SportsMan(cyclicBarrier);
+    }
+
+    static class Run extends Thread {
+
+        @Override
+        public void run() {
+            System.out.println("run is begun");
+        }
+    }
+
+    static class SportsMan extends Thread {
+        CyclicBarrier cyclicBarrier;
+
+        public SportsMan(CyclicBarrier cyclicBarrier) {
+            this.cyclicBarrier = cyclicBarrier;
+            start();
+        }
+
+        @Override
+        public void run() {
+            try {
+                cyclicBarrier.await();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } catch (BrokenBarrierException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+}
