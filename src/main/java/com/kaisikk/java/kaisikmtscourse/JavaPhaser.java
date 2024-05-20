5165545d23dfd2effdd4abfdbd46d166dd667720
Kaisikk <Kosikov.Ilya@mail.ru>
2024-05-20 21:49:45 +0400

    Работа с Phaser

---
+++ src/main/java/com/kaisikk/java/kaisikmtscourse/JavaPhaser.java
@@ -0,0 +1,31 @@
+package com.kaisikk.java.kaisikmtscourse;
+
+import java.util.concurrent.Phaser;
+
+public class JavaPhaser {
+
+    public static void main(String[] args) {
+        Phaser phaser = new Phaser(2);
+        new Washer(phaser);
+        new Washer(phaser);
+    }
+
+    static class Washer extends Thread {
+        Phaser phaser;
+
+        public Washer(Phaser phaser) {
+            this.phaser = phaser;
+            start();
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 3; i++) {
+                System.out.println(getName() + " washing the car");
+                phaser.arriveAndAwaitAdvance();
+            }
+
+        }
+    }
+
+}
