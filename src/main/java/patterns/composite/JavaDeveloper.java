dc52f1f05a2af1e600b3577735df58a902ea1e9e
Kaisikk <Kosikov.Ilya@mail.ru>
2024-08-03 20:50:12 +0400

    Реализация паттерна Composite

---
+++ src/main/java/patterns/composite/JavaDeveloper.java
@@ -0,0 +1,8 @@
+package patterns.composite;
+
+public class JavaDeveloper implements Developer {
+    @Override
+    public void writeCode() {
+        System.out.println("Write Java code");
+    }
+}
