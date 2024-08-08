771de8cca066534e91629a6c6da3dc779d9de6b9
Kaisikk <Kosikov.Ilya@mail.ru>
2024-08-08 22:58:35 +0400

    Реализация паттерна Chain of Responsibility

---
+++ src/main/java/patterns/chainofresponsibility/EmailNotifier.java
@@ -0,0 +1,13 @@
+package patterns.chainofresponsibility;
+
+public class EmailNotifier extends Notifier {
+
+    public EmailNotifier(int priority) {
+        super(priority);
+    }
+
+    @Override
+    public void write(String message) {
+        System.out.println("Sending email: " + message);
+    }
+}
